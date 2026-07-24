7839fdf94dcdeba42e4dcac0426681b8567561a3
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-07-23 21:35:12 -0500

    Add: reverse-nodes-in-k-group - submission-3

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-3.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        temp = head
+        start = temp
+        counter = 1
+        while temp:
+            if counter % k == 0:
+                after = start.next
+                #sever the kth node from rest of list temporarily
+                temp.next = None
+                #create a pointer to what would be the node after kth node
+                #after_temp = temp.next
+
+                #return the head of the severed, now reversed linked list
+                current_reversal = self.reverse_linked_list(start)
+
+                new_temp = current_reversal
+
+                while new_temp.next:
+                    new_temp = new_temp.next
+                
+                new_temp.next = after
+                start = after
+                
+            temp = temp.next
+            counter+=1
+
+
+
+    
+    def reverse_linked_list(self, node):
+
+        temp = node
+        before = None
+
+        while temp:
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+        #before is where the last pointer ended up.
+        return before
+
+        
