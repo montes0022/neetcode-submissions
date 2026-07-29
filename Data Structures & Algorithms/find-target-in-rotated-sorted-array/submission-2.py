bf4c75fdacae2fcbd4ee0d66c8b9d4ff96b55a3c
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-07-29 18:48:36 -0500

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+
+        smallest = nums[0]
+
+        while left <= right:
+            print(f'l:{left} and r:{right}')
+            if nums[left] < nums[right]:
+                #if the left most is smaller than the right most
+                #array is sorted, compare result to leftmost index and break
+                return left
+
+            #compute mid
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            #target is probably in the right segment
+            if nums[mid] > target:
+                left = mid + 1
+            #target must be in the left segment
+            elif nums[mid] < target:
+                right = mid - 1
+
+
+        return -1
+        
