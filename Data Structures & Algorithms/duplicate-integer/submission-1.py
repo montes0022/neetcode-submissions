7d66319c4dbb591788413c123c1c7bd52af1fa8b
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-08-09 20:27:02 -0500

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        store = {}
+
+        for item in nums:
+            if item not in store:
+                store[item] = True
+            else:
+                return False
+
+        return True
+
+
+        
