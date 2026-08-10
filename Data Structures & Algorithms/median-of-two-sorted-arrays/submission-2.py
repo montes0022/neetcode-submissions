88b7068b31b3906586cf037ce5590c5112180dd9
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-08-09 19:36:00 -0500

    Add: median-of-two-sorted-arrays - submission-2

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-2.py
@@ -0,0 +1,49 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        a = nums1
+        b = nums2
+
+        total = len(nums1) + len(nums2)
+        #half is roughly the number of elements in the left partition.
+        half = total // 2
+
+        #if a is ever bigger than b, swap them.
+        if len(b) < len(a):
+            a,b = b,a
+
+        l = 0
+        r = len(a) - 1
+
+        #bst, normally l<=r but our edge case is finding the median which
+        #will break us out of the loop.
+        while True:
+            m = (l+r) // 2 #a's left partition last position
+
+            x = half - m - 2 #b's left partition element count, subtract 2 because of indexing
+
+            #leftmost item in a's left partition
+            Aleft = a[m]
+            #rightmost item in a's right partition
+            Aright = a[m+1]
+
+            #leftmost item in b's left partition
+            Bleft = a[x]
+            #rightmost item in b's right partition
+            Bright = a[x+1]
+
+            #this means left partition of merged array is valid.
+            if Aleft <= Bright and Bleft <= Aright:
+                #depending on if total was even or odd...
+                #odd, get the min of Aright and Bright
+                #even, max(Aleft,Bleft) + min(Aright, Bright) / 2
+                if number % 2 == 0:
+                    return max(Aleft,Bleft) + min(Aright, Bright) / 2
+                else:
+                    return min(Aright, Bright)
+            else: #we need to move up the left partition in A by one to see
+            #if getting the new left partition of B makes a valid combined left 
+            #partition from both arrays
+                l = m +1
+
+
+
