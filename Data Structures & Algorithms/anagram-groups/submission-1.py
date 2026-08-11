c79446a52fa60158d9643a2b9aeb004b103ba219
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-08-10 21:09:27 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groupings = []
+        anagram_dict = {}
+
+        for word in strs:
+            #this sorts the current word in alpha order
+            sorted_string = ''.join(sorted(word))
+
+            #make a key of the word in alpha order, so that 
+            #future words that are angrams of it match the key when sorted.
+            if sorted_string not in anagram_dict:
+                #create a new list at that sorted key
+                anagram_dict[sorted_string] = []
+            #append the word to the key which is that word sorted.
+            anagram_dict[sorted_string].append(word)
+
+        for sorted_key in anagram_dict:
+            current_grouping = []
+
+            for word in anagram_dict[sorted_key]:
+                current_grouping.append(word)
+            
+            groupings.append(current_grouping)
+
+        return groupings
+        
