72f1011f4e1114c76b228f5157098c7a292a5e1b
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-08-11 21:11:54 -0500

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        my_dict = {}
+        #need an extra index to hold the frequency of n where n is len(nums)
+        buckets = [[] for _ in range(len(nums))+1]
+
+        #hashmap of each number and its frequency in nums
+        for item in nums:
+            my_dict[item] = 1 + my_dict.get(item, 0)
+
+        #v is the frequency of k in nums
+        for k, v in my_dict.items():
+            my_dict[v].append(k)
+
+        res = []
+        for i in range(len(buckets)-1, 0, -1):
+            #item is the number that appears i times in nums
+            #automatically add the collection at buckets[i]
+            #since we start at the top, the most frequent items.
+            for item in buckets[i]:
+                res.append(item)
+                
+                if len(res) == k:
+                    return res
