03a01a0be845cf62d5d8dff8e0585ed3d9c1806b
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-08-12 22:57:38 -0500

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        #at least i got this initialization right
+        output = [1] * len(nums)
+
+        #prefix loop.
+        pre = 1
+
+        for i in range(len(nums)-1):
+            res[i] = pre
+            
+            #update prefix so that prefix is the product of itself ->
+            #and the current element i in nums
+            pre *= nums[i]
+
+        #postfix loop
+        post = 1
+
+        #for i in range(len(nums)-1, 0, -1): you did this
+        #the middle arg is non inclusive, so you would have stopped at second to last.
+        for i in range(len(nums)-1, -1, -1):
+            #multiply post fix with the value that is already in result.
+            res[i] *= post
+
+            post *= nums[i]
+
+        return res
+
+        
