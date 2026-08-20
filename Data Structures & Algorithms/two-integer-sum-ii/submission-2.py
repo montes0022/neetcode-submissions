e8744d8fb153df02c3e3d6cafb7a9fed66a619b6
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-08-19 22:37:46 -0500

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        start = 0
+        end = len(numbers) -1
+
+        while start <= end:
+            if numbers[start] + numbers[end] == target:
+                return [start+1, end+1]
+
+            if numbers[start] + numbers[end] > target:
+                #nums at end must be excluded because the biggest number
+                #cant be apart of the solution if its sum with smallest
+                #number exceed target.
+                end -= 1
+
+            else:
+                start += 1
+
+            
+        
