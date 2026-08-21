44ca67c2871a69ebe072b00850108e586e5062dc
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-08-20 21:40:37 -0500

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        sorted_nums = nums.sort()
+        print(nums)
+        print(sorted_nums)
+
+        return output
+        #for i in range(len(nums)):
+
+
+
+
+        
+        
