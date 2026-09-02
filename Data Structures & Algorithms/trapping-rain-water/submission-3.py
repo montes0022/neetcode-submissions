d12e05272461aefe06ecd1e9e4bad715508463bd
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-09-01 20:59:42 -0500

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        def threeSum(self, nums: List[int]) -> List[List[int]]:
+            water = 0
+
+            start = 0
+            end = len(nums) -1
+
+            leftmax = height[start]
+            rightmax = height[end]
+            
+            while start < end:
+
+                #smaller max value gets shifted inward.
+                #the formula is MIN(LEFTMAX, RIGHTMAX)
+                #whatever condition you go to in here is the min!
+                if leftmax < rightmax:
+                    #move left over first
+                    start += 1
+                    #calculate water before updating leftmax
+                    water += max(0, leftmax - height[start])
+                    leftmax = max(leftmax, height[start])
+                else:
+                    end -= 1
+                    water += max(0, rightmax - height[end])
+                    rightmax = max(rightmax, height[end])
+
+            return water
+
+        
