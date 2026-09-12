1bbc34a0456255ed4d449cb0220aa124c00b06dc
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-09-12 14:32:49 -0500

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        chars = {}
+        l = 0
+        window = 0
+    
+        for r in range(len(s)):
+            if s[r] in chars:
+                chars[s[r]] += 1
+            else:
+                chars[s[r]] = 1
+            max_value = max(chars.values())
+            current_window = ((r-l) + 1)
+    
+            #if our number of replacements ever exceeds k, move l over.
+            #Then the window is valid — you can make all characters 
+            #identical with ≤ k repl acements.
+            #If it’s not valid, you shrink the window from the left.
+            replacements = current_window - max_value
+    
+            if k < replacements:
+                l += 1
+                current_window = ((r-l) + 1)
+                window = max(window, current_window)
+                return window
+            
+            window = max(window, current_window)
+        return window  
+        
+        
