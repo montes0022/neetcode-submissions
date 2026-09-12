c4f67d303e2cc1164c518627fe385ec2c5480287
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-09-12 16:56:11 -0500

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        window = len(s1)
+        ordered = ''.join(sorted(s1))
+
+        for l in range(len(s2)):
+            chunk = s2[l:l+window]
+
+            if ''.join(sorted(chunk)) == ordered:
+                return True
+
+        return False
+        
