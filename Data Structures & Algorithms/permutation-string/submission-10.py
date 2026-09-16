bdd387b522a57c1e25735953a3777d84ba424342
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-09-15 21:42:16 -0500

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        countS1 = {}
+        countS2 = {}
+
+        for i in range(len(s1)):
+            #always add 1 to the key of every letter that appears in each string
+            countS1[s1[i]] = 1 + countS1.get(s1[i], 0)
+            countS2[s2[i]] = 1 + countS2.get(s2[i], 0)
+
+        l = 0
+
+        for r in range(len(s1), len(s2)):
+            if countS1 == countS2:
+                return True
+
+            countS2[s2[r]] = 1 + countS2.get(s2[r], 0)
+
+            countS2[s2[l]] = countS2.get(s2[l], 0) - 1
+
+            if countS1 == countS2:
+                return True
+
+            l += 1
+
+        if countS1 == countS2:
+                return True
+
+            
+
+        
