b094b36d9ded23a5bbca6f624c8b7be1c11a6b93
montes0022 <73719973+montes0022@users.noreply.github.com>
2026-09-21 18:28:34 -0500

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        output = 0
+        #do we need to keep track of the frequencies of the 
+        #characters in t?
+
+
+
+        #the output will at least be the length of s
+        output = len(s)
+        
